d1104af2e8d40adeeb3dee5c1c702f67f0ef90c1
Raul Silva <silvamraul709@gmail.com>
2026-06-28 18:05:32 -0500

    primer commit

---
+++ space-reservation-api/src/main/java/com/space_reservation/api/SpaceReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.space_reservation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceReservationApiApplication.class, args);
+	}
+
+}
---
+++ space-reservation-api/src/test/java/com/space_reservation/api/SpaceReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space_reservation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
